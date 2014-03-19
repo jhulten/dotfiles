8a13a972359ca170142b06139169bb04bb143d01
Jeffrey Hulten <jhulten@whitepages.com>
2014-03-18 18:04:06 -0700

    checkpoint

---
+++ shell/zsh/aliases.zsh
@@ -0,0 +1,5 @@
+# User Aliased
+#alias knife='nocorrect knife'
+#alias rake="noglob rake"
+alias t=todo.sh
+alias git=hub
---
+++ shell/zsh/oh-my-zsh.zsh
@@ -0,0 +1,9 @@
+export ZSH=$HOME/.oh-my-zsh
+
+ZSH_THEME="jhulten"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git osx ruby knife brew bundler lein rbenv ec2)
+
+source $ZSH/oh-my-zsh.sh

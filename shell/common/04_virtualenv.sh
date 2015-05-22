99ae3a3c859e1ebf803a765dff259aa81084a2ff
Jeffrey Hulten <jeff@datadoghq.com>
2015-05-22 00:29:24 -0400

    clean up if/fi block

--- shell/common/04_virtualenv.sh
+++ shell/common/04_virtualenv.sh
@@ -1,6 +1,8 @@
 if which pyenv >/dev/null 2>&1; then
   echo Enabling pyenv...
   export WORKON_HOME=$HOME/.virtualenvs
-  eval "$(pyenv init -)"; fi
-  if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+  eval "$(pyenv init -)"
+  if which pyenv-virtualenv-init > /dev/null; then
+    eval "$(pyenv virtualenv-init -)"
+  fi
 fi

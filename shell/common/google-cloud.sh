072b65ebf2e3c6d08d2fc0fb127a59fa1684332b
Jeffrey Hulten <jeff@datadoghq.com>
2015-05-24 11:46:50 -0700

    Disable gce completion and paths

--- shell/common/google-cloud.sh
+++ shell/common/google-cloud.sh
@@ -1,7 +1,7 @@
-if [[ -d /opt/homebrew-cask/Caskroom/google-cloud-sdk/latest ]]; then
-  echo Update PATH for Google Cloud SDK....
-  source /opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.$(basename $SHELL).inc
-
-  echo Enable shell completion for gcloud...
-  source /opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.$(basename $SHELL).inc
-fi
+# if [[ -d /opt/homebrew-cask/Caskroom/google-cloud-sdk/latest ]]; then
+#   echo Update PATH for Google Cloud SDK....
+#   source /opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.$(basename $SHELL).inc
+#
+#   echo Enable shell completion for gcloud...
+#   source /opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.$(basename $SHELL).inc
+# fi

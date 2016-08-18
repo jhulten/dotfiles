6594917874a282e650fc3276337ac18c1e7dbd3d
Jeffrey Hulten <jeffrey@moz.com>
2016-08-18 16:23:26 -0700

    disable auto install of homebrew
    `lockfile` is not available on macosx

--- shell/common/01_homebrew.sh
+++ shell/common/01_homebrew.sh
@@ -1,15 +1,15 @@
 # Homebrew
 if [[ "$platform" == 'macosx' ]]; then
-  HOMEBREW_LOCK=/tmp/homebrew.install
-
-  lockfile -2 -r 30 $HOMEBREW_LOCK || exit 1
-
-  if [[ ! -f /usr/local/bin/brew ]]; then
-    echo "Installing Homebrew..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  fi
-
-  rm -f $HOMEBREW_LOCK
+  # HOMEBREW_LOCK=/tmp/homebrew.install
+  #
+  # lockfile -2 -r 30 $HOMEBREW_LOCK || exit 1
+  #
+  # if [[ ! -f /usr/local/bin/brew ]]; then
+  #   echo "Installing Homebrew..."
+  #   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  # fi
+  #
+  # rm -f $HOMEBREW_LOCK
 
   # Python Home
   export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages

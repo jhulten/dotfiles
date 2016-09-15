15249e63ba7932e516fa1fd2e518ac7e75eaf775
Jeffrey Hulten <jeffrey@moz.com>
2016-09-15 13:57:39 -0700

    Checkpoint

---
+++ systems/moz/shell.sh
@@ -0,0 +1,5 @@
+SEOMOZ_ROOT=~/src/github.com/seomoz
+alias edit_secrets='ansible-vault edit ~/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass'
+alias fetch_secrets='pushd $SEOMOZ_ROOT/roger && ansible-playbook -i dalmesos01, ansible/ansible-secrets-fetch.yml && popd'
+alias push_secrets='pushd $SEOMOZ_ROOT/roger && ansible-playbook -i dalmesos01, ansible/ansible-secrets-push.yml && popd'
+alias view_secrets='ansible-vault view ~/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass'

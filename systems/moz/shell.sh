45254cd4bfc552cbd1a0f2344094450f4b2c6445
Jeffrey Hulten <jeffrey@moz.com>
2016-10-18 12:59:22 -0700

    Add chruby default

--- systems/moz/shell.sh
+++ systems/moz/shell.sh
@@ -3,3 +3,5 @@ alias edit_secrets='ansible-vault edit ~/rogeros-ansible-secrets.yml.encrypted -
 alias fetch_secrets='pushd $SEOMOZ_ROOT/roger && ansible-playbook -i ansible/hosts/dev-1 ansible/ansible-secrets-fetch.yml && popd'
 alias push_secrets='pushd $SEOMOZ_ROOT/roger && ansible-playbook -i ansible/hosts/dev-1 ansible/ansible-secrets-push.yml && popd'
 alias view_secrets='ansible-vault view ~/rogeros-ansible-secrets.yml.encrypted --vault-password-file ~/.rogeros-ansible-vault-pass'
+
+chruby chefdk

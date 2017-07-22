8d3f51248aa5c375c112b41a7185f41f5cca66e7
Jeffrey Hulten <jeffrey@moz.com>
2017-07-22 15:13:00 -0700

    Switch away from graphical vim

--- shell/common/01_editor.sh
+++ shell/common/01_editor.sh
@@ -1,19 +1,6 @@
 # EDITOR
 
-if [ -z "$SSH_CONNECTION" ]; then
-   case $platform in
-   macosx*)
-       export EMACSCLIENT=/usr/local/bin/emacsclient
-       alias emacsclient=$EMACSCLIENT
-       ;;
-   *)
-       export EMACSCLIENT=emacsclient
-       ;;
-   esac
-   alias ec="$EMACSCLIENT -c -n"
-   export EDITOR="gvim -f"
-   export ALTERNATE_EDITOR="vim"
-else
-    export EDITOR=$(type -P vim || type -P vi)
-fi
+export ALTERNATE_EDITOR="vim"
+export EDITOR=$(type -P vim || type -P vi)
+
 export VISUAL=$EDITOR

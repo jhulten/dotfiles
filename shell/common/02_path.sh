f542a6f07b5ad9afc3777300e0fc94a48b929ae5
Jeffrey Hulten <jeffrey.hulten@gettyimages.com>
2018-07-05 16:12:27 -0700

    checkpoint

--- shell/common/02_path.sh
+++ shell/common/02_path.sh
@@ -5,3 +5,9 @@ export CDPATH=$HOME/src/gitlab.amer.gettywan.com:$HOME/src/github.com:$HOME/src/
 function gocd () {
   cd $(go list -f '{{.Dir}}' $1)
 }
+
+# Find your path Python User Base path (where Python --user will install packages/scripts)
+USER_BASE_PATH="$(python -m site --user-base)"
+
+# Add this path to your $PATH
+export PATH=$USER_BASE_PATH/bin:$PATH

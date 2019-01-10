0ca6b8b418ed388a3565000dfed69e2144f38a17
Jeffrey Hulten <jeffrey.hulten@gettyimages.com>
2019-01-09 20:06:17 -0800

    shell settings

--- shell/common/01_homebrew.sh
+++ shell/common/01_homebrew.sh
@@ -1,31 +1,40 @@
 # Homebrew
 
-if [[ "$platform" == "macosx" ]]; then
-  # HOMEBREW_LOCK=/tmp/homebrew.install
-  #
-  # lockfile -2 -r 30 $HOMEBREW_LOCK || exit 1
-  #
-  # if [[ ! -f /usr/local/bin/brew ]]; then
-  #   echo "Installing Homebrew..."
-  #   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  # fi
-  #
-  # rm -f $HOMEBREW_LOCK
+if [[ -x "/usr/local/bin/brew" || -x "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
 
-  # Python Home
-  #export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+  if [[ "$platform" == "macosx" ]]; then
+    # HOMEBREW_LOCK=/tmp/homebrew.install
+    #
+    # lockfile -2 -r 30 $HOMEBREW_LOCK || exit 1
+    #
+    # if [[ ! -f /usr/local/bin/brew ]]; then
+    #   echo "Installing Homebrew..."
+    #   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    # fi
+    #
+    # rm -f $HOMEBREW_LOCK
 
-  if [ -f $(brew --prefix)/etc/bash_completion ]; then
-    echo "enabling completions from homebrew"
-    if [[ "$(basename $SHELL)" == "bash" ]]; then
-      . $(brew --prefix)/etc/$(basename $SHELL )_completion
-    elif [[ "$(basename $SHELL)" == "zsh" ]]; then
-      fpath=(/usr/local/share/zsh-completions $fpath)
-    else
-      echo "completions not configured for $SHELL"
+    # Python Home
+    #export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+
+    if [ -f $(brew --prefix)/etc/bash_completion ]; then
+      echo "enabling completions from homebrew"
+      if [[ "$(basename $SHELL)" == "bash" ]]; then
+        . $(brew --prefix)/etc/$(basename $SHELL )_completion
+      elif [[ "$(basename $SHELL)" == "zsh" ]]; then
+        fpath=(/usr/local/share/zsh-completions $fpath)
+      else
+        echo "completions not configured for $SHELL"
+      fi
+
     fi
-
+  elif [[ "$platform" == "linux" ]]; then
+    brewhome=/home/linuxbrew
+    export PKG_CONFIG_PATH="/usr/bin/pkg-config:$brewhome/.linuxbrew/bin/pkg-config" 
+    export PKG_CONFIG_LIBDIR="/usr/lib/pkgconfig:$brewhome/.linuxbrew/lib/pkgconfig" 
+    export PATH="$brewhome/.linuxbrew/bin:$PATH"
+    export MANPATH="$brewhome/.linuxbrew/share/man:$MANPATH"
+    export INFOPATH="$brewhome/.linuxbrew/share/info:$INFOPATH"
   fi
-else
-  echo "Skipping homebrew configuration for $platform"
+
 fi
--- shell/common/02_path.sh
+++ shell/common/02_path.sh
@@ -1,13 +1,13 @@
 echo Updating PATH...
 export PATH=$PATH:~/bin
-export CDPATH=$HOME/src/gitlab.amer.gettywan.com:$HOME/src/github.com:$HOME/src/code.google.com/p
+export CDPATH=$HOME/src/gitlab.getty.cloud:$HOME/src/github.com:$HOME/src/code.google.com/p
 
 function gocd () {
   cd $(go list -f '{{.Dir}}' $1)
 }
 
 # Find your path Python User Base path (where Python --user will install packages/scripts)
-USER_BASE_PATH="$(python -m site --user-base)"
+#USER_BASE_PATH="$(python -m site --user-base)"
 
 # Add this path to your $PATH
-export PATH=$USER_BASE_PATH/bin:$PATH
+#export PATH=$USER_BASE_PATH/bin:$PATH

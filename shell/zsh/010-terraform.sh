6424843d7d9a3ae4014ea86b367b15af3eb54bc4
Jeffrey Hulten <jeffrey.hulten@gettyimages.com>
2020-02-20 12:22:09 -0800

    terraform state prompt

---
+++ shell/zsh/010-terraform.sh
@@ -0,0 +1,12 @@
+zsh_terraform() {
+  # break if there is no .terraform directory
+  if [[ -d .terraform ]]; then
+    local tf_workspace=$(/usr/local/bin/terraform workspace show)
+    echo -n "$tf_workspace"
+  fi
+}
+
+# Terraform Segment
+POWERLEVEL9K_CUSTOM_TERRAFORM="zsh_terraform"
+POWERLEVEL9K_CUSTOM_TERRAFORM_BACKGROUND=057
+POWERLEVEL9K_CUSTOM_TERRAFORM_FOREGROUND=015

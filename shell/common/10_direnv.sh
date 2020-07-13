d052eb1c8b7dc0783c8cbd779695c317f10c08ea
Jeffrey Hulten <jeffrey.hulten@gettyimages.com>
2020-07-13 10:45:59 -0700

    add install of direnv

--- shell/common/10_direnv.sh
+++ shell/common/10_direnv.sh
@@ -1,6 +1,13 @@
 if which direnv >/dev/null 2>&1; then
 else
   echo "missing direnv"
+  if [[ $HAS_BREW == 1 ]]; then
+    echo "installing via homebrew"
+    brew install direnv
+  elif which apt-get >/dev/null 2>&1; then
+    echo "installing via apt-get"
+    apt-get update && apt-get install -y direnv
+  fi
 fi
 
 if which direnv >/dev/null 2>&1; then

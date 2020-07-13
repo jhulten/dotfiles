3e316bb5bfaa7627ac69bd105daaa75b45840410
Jeffrey Hulten <jeffrey.hulten@gettyimages.com>
2020-07-13 10:18:06 -0700

    prepping direnv install changes

--- shell/common/10_direnv.sh
+++ shell/common/10_direnv.sh
@@ -1,4 +1,9 @@
 if which direnv >/dev/null 2>&1; then
+else
+  echo "missing direnv"
+fi
+
+if which direnv >/dev/null 2>&1; then
   echo "enabling direnv"
   eval "$(direnv hook `basename $SHELL`)"
 fi
